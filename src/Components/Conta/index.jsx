20d92357d766a4a3be837846fbc8631b544a68b1
Leonardo Scalabrin <leojscalabrin@gmail.com>
2021-11-06 18:45:55 -0300

    extrato e ajustes

--- src/Components/Conta/index.jsx
+++ src/Components/Conta/index.jsx
@@ -5,11 +5,11 @@ import privado from "../../assets/images/privado.svg";
 import olho_icone from "../../assets/images/olho.svg";
 import dinheiro from "../../assets/images/dinheiro.svg";
 
-import { Icone, Box, Botao, Saldo, Detalhe} from "../../Components/UI";
+import { Icone, Box, Botao, Saldo, Detalhe } from "../../Components/UI";
 
 const IconeMargin = styled(Icone)`
   margin-top: 2px;
-`
+`;
 
 const Conta = () => {
   const [toggleState, untoggle] = useState(true);
--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 import Titulo from "../Titulo";
 import Conta from "../Conta";
+import Extrato from "../Extrato";
 
 const Container = styled.div`
   background-color: #f1f1f1;
@@ -20,12 +21,13 @@ const Conteudo = styled.section`
   }
 `;
 
- export default () => {
+export default () => {
   return (
     <Container>
       <Titulo>Olá Fulano!</Titulo>
       <Conteudo>
         <Conta />
+        <Extrato />
       </Conteudo>
     </Container>
   );
